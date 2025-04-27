5c2160ccf4b08a751c370411e829f55206ae887b
Ian Liu <ianliu@livemail.tw>
2025-04-27 19:55:09 +0800

    feature: add ability to remove selected annotation

--- src/Annotation.tsx
+++ src/Annotation.tsx
@@ -1,27 +1,39 @@
 import { useState, useEffect } from 'react';
-import { useSelection, useAnnotation } from '@annotorious/react';
+import { useSelection, useAnnotation, ImageAnnotation, useAnnotations, useAnnotator } from '@annotorious/react';
 
 // This component uses the Annotorious hooks
 export default function Annotation() {
     const { selected, event } = useSelection();
     const [targetAnnotationId, setTargetAnnotationId] = useState<string | undefined>(undefined);
-
+    const annotations: ImageAnnotation[] = useAnnotations();
+    const anno = useAnnotator();
+    
+    function removeAnnotation() {
+        anno.removeAnnotation(targetAnnotationId ?? "");
+    }
     useEffect(() => {
-        // console.log('useSelection hook updated. Selected:', selected, 'Event:', event); // Keep logs for debugging
         if (selected && selected.length > 0) {
             setTargetAnnotationId(selected[0].annotation.id);
         } else {
             setTargetAnnotationId(undefined);
         }
     }, [selected, event]);
-
+    
     return (
         // Area to display the details of the selected annotation
         <div style={{ marginTop: '20px', border: '1px solid #ccc', padding: '10px' }}>
+            <button onClick={removeAnnotation}> Remove selected annotation!</button>
             <div>{targetAnnotationId}</div>
-            <h2>Selected Annotation Details</h2>
+            <div>
+                <ul>
+                    {annotations.map(a => (
+                        <li key={a.id}>{a.id}</li>
+                    ))}
+                </ul>
+            </div>
             <AnnotationDetails key={targetAnnotationId} id={targetAnnotationId} />
         </div>
+
     );
 }
 
@@ -29,6 +41,7 @@ function AnnotationDetails({ id }: { id: string | undefined }) {
     const annotationDetails = useAnnotation(id ?? "");
 
     return (<div>
+        <h2>Selected Annotation Details</h2>
         {annotationDetails ? (
             <pre>{JSON.stringify(annotationDetails, null, 2)}</pre>
         ) : (
